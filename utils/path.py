d5d38c6161d50c8e93932e7e7e01f8eadd559703
kristina-albrecht <kristina.a.albrecht@gmail.com>
2021-04-17 21:26:29 +0200

    Add util function to create directories and files if not already exist

---
+++ utils/path.py
@@ -0,0 +1,11 @@
+import errno
+import os
+
+
+def create_if_not_exists(path):
+    if not os.path.exists(os.path.dirname(path)):
+        try:
+            os.makedirs(os.path.dirname(path))
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
